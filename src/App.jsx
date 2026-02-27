807a633201c28353b31a74caa50ad73db43a1511
Wei-Ting Yap <29475856+weitingyp@users.noreply.github.com>
2026-02-27 16:59:53 +0800

    feat: fetch 12 pokemons using pokeapi

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,43 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
+
+  // Fetch 12 pokemon data from pokeapi
+  const pokemonId = Array.from({ length: 12 }, (_, i) => (i + 1) * 10);
+  const [ library, setLibrary ] = useState([]);
+
+  useEffect(()=>{
+    async function initializeLibrary() {
+      return await Promise.all(
+        pokemonId.map( async (id) => {
+          const response = await fetch('https://pokeapi.co/api/v2/pokemon/'+id);
+          const result = await response.json();
+          return {
+            name: result.forms[0].name,
+            id: result.id,
+            url: result.sprites.front_default,
+            isPicked: false
+          }
+        })
+      )
+    }
+    initializeLibrary().then( (initLibrary) => setLibrary(initLibrary) );
+  }, [])
+
   return (
-    <>
-      <h1>Memory Card Game</h1>
-    </>
-  )
+    <div id="gameboard">
+      {
+        library.length !== 0 && library.map( pokemon => {
+          return (
+            <div key={pokemon.id} className='pokemon-card'>
+              <h2>{pokemon.name}</h2>
+              <img src={pokemon.url} alt={pokemon.name} />
+            </div>
+          )
+        })
+      }
+    </div>)
 }
 
 export default App
